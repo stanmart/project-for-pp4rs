f4dd603ab0a9cda76ea929bd26fc02a0e89c9bf3
Martin Stancsics <martin.stancsics@gmail.com>
2020-02-26 21:53:34 +0100

    Added a script for the main plot

---
+++ src/figures/plot_fire.py
@@ -0,0 +1,73 @@
+import argparse
+import pandas as pd
+import colorcet as cc
+import datashader as ds
+import datashader.transfer_functions as tf
+from datashader.utils import export_image
+
+
+def create_plot(data, out, width):
+    """Creates a figure of the ZVV transit network without any grouping.
+
+    Args:
+        data: a csv file containing data usable for line plots
+        out: the generated imnage is saved here
+
+    Returns:
+        None
+    """
+
+    plot_data = pd.read_csv(data, low_memory=False)
+
+    x_range = (plot_data.shape_pt_lon.min(), plot_data.shape_pt_lon.max())
+    y_range = (plot_data.shape_pt_lat.min(), plot_data.shape_pt_lat.max())
+
+    height = round(width * (y_range[1] - y_range[0]) / (x_range[1] - x_range[0]))
+
+    cvs = ds.Canvas(
+        plot_width=width,
+        plot_height=height,
+        x_range=x_range,
+        y_range=y_range
+    )
+    agg = cvs.line(
+        plot_data, 'shape_pt_lon', 'shape_pt_lat',
+        agg=ds.sum('times_taken')
+    )
+    image = tf.shade(agg, cmap=cc.fire, how='eq_hist')
+
+    if out.endswith('.png'):
+        out = out[:-4]
+    export_image(image, filename=out, background='black')
+
+
+def main():
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        '-d', '--data',
+        help="A line-plot-compatible data file",
+        type=str,
+        required=True
+    )
+    parser.add_argument(
+        '-o', '--out',
+        help="The path of the output file",
+        type=str,
+        required=True
+    )
+    parser.add_argument(
+        '-w', '--width',
+        help="The width of the image in pixels",
+        type=int,
+        default=1200
+    )
+
+    args = parser.parse_args()
+
+    create_plot(args.data, args.out, args.width)
+
+
+if __name__ == "__main__":
+    main()
