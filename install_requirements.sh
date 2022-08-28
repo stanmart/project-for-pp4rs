dba960261e581bd727fe5cce88714b553c7581a7
Martin Stancsics <martin.stancsics@gmail.com>
2022-08-28 16:23:29 +0200

    Switch to environment.yml for specifying deps

--- install_requirements.sh
+++
@@ -1,7 +0,0 @@
-conda create --prefix ./venv python=3.7 \
-             --file ./requirements.txt \
-             --channel conda-forge \
-             --channel bioconda \
-             --yes && \
-conda activate ./venv && \
-pip install -r ./pip-requirements.txt
